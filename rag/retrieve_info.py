b0d03b2ae3b4451bb0ce9b830a8cf5233f25b848
Kanka Oza <kankaoza1609@gmail.com>
2026-03-01 22:41:13 +0530

    ok

--- rag/retrieve_info.py
+++ rag/retrieve_info.py
@@ -1,7 +1,20 @@
 class retriever:
-    def __init__(self, vectordb, embedder):
+    def __init__(self,vectordb,embedder,fuzzy_matcher):
         self.vectordb=vectordb
         self.embedder=embedder
+        self.fuzzy=fuzzy_matcher
+
     def retrieve(self, ques, k=20):
-        ques_embed=self.embedder.embedtext([ques])
-        return self.vectordb.search(ques_embed,k)    
+
+        #original query
+        original_embed=self.embedder.embedtext([ques])
+
+        #best corrected query
+        corrected=self.fuzzy.correct(ques)
+        corrected_embed=self.embedder.embedtext([corrected])
+
+        #searchin using both
+        res1=self.vectordb.search(original_embed, k)
+        res2=self.vectordb.search(corrected_embed, k)
+
+        return list(set(res1+res2))
